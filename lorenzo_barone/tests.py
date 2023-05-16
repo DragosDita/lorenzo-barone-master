5a6c4f820e2db762b5520c0e413b30604c77a005
dragos.dita <94638237+DragosDita@users.noreply.github.com>
2023-05-16 15:40:15 +0300

    Added a test case 6 TestCases and 1/6 failed `1+2=2` and modified the .yaml file

--- lorenzo_barone/tests.py
+++ lorenzo_barone/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+class lorenzo_baroneTestCase(TestCase):
+    def test_basic_addition(self):
+        # Basic addition test
+        result = 1 + 2
+        self.assertEqual(result, 2, "Addition result is incorrect")
+
+    def test_negative_numbers(self):
+        # Test negative numbers
+        result = -5 + 10
+        self.assertEqual(result, 5, "Addition of negative numbers is incorrect")
+
+    def test_addition_with_zero(self):
+        # Test zero
+        result = 0 + 100
+        self.assertEqual(result, 100, "Addition with zero is incorrect")
+
+    def test_large_numbers(self):
+        # Test large numbers
+        result = 999999999 + 1
+        self.assertEqual(result, 1000000000, "Addition of large numbers is incorrect")
+
+    def test_floating_point_addition(self):
+        # Test floating-point addition
+        result = 1.5 + 2.3
+        self.assertAlmostEqual(result, 3.8, places=2, msg="Floating-point addition is incorrect")
+
+    def test_string_concatenation(self):
+        # Test string concatenation
+        result = "Hello" + " " + "World"
+        self.assertEqual(result, "Hello World", "String concatenation is incorrect")
