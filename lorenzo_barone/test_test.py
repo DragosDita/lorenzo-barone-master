2d1ec02f07770e09b70626e414a05fb02f91ac47
dragos.dita <94638237+DragosDita@users.noreply.github.com>
2023-05-17 11:49:35 +0300

    retry to have succesfully pipeline

--- lorenzo_barone/test_test.py
+++ lorenzo_barone/test_test.py
@@ -5,29 +5,35 @@ class TestLorenzoBarone(TestCase):
     def test_basic_addition(self):
         # Basic addition test
         result = 1 + 1
+        print("1 + 1 =", result)
         self.assertEqual(result, 2, "1 + 1 = 2")
 
     def test_negative_numbers(self):
         # Test negative numbers
         result = -5 + 10
+        print("-5 + 10 =", result)
         self.assertEqual(result, 5, "-5 + 10 = 5")
 
     def test_addition_with_zero(self):
         # Test zero
         result = 0 + 100
+        print("0 + 100 =", result)
         self.assertEqual(result, 100, "0 + 100 = 100")
 
     def test_large_numbers(self):
         # Test large numbers
         result = 999999999 + 1
+        print("999999999 + 1 =", result)
         self.assertEqual(result, 1000000000, "999999999 + 1 = 1000000000")
 
     def test_floating_point_addition(self):
         # Test floating-point addition
         result = 1.5 + 2.3
+        print("1.5 + 2.3 =", result)
         self.assertAlmostEqual(result, 3.8, places=2, msg="1.5 + 2.3 = 3.8")
 
     def test_string_concatenation(self):
         # Test string concatenation
         result = "Hello" + " " + "World"
+        print("'Hello' + ' ' + 'World' =", result)
         self.assertEqual(result, "Hello World", "'Hello' + ' ' + 'World' = 'Hello World'")
